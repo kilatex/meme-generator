da9e5957f185754bae34b8625b74052d83330c30
Santiago <santiagodmaldon@gmail.com>
2022-10-05 12:44:33 -0400

    memes.js added

---
+++ src/assets/memes.js
@@ -0,0 +1,24 @@
+const memes = [
+    {
+        id : 1,
+        title: 'Shut up and take my money',
+        image: 'https://i.pinimg.com/originals/77/a6/28/77a628cb2ccbcc26a7ed6fdc737e31c7.jpg'
+    },
+    {
+        id : 2,
+        title: 'Señora llorando y gato',
+        image: 'https://www.infobae.com/new-resizer/pa4_8hmGlNWiwcD8m2Ere1LxV5k=/1200x628/filters:format(webp):quality(85)//s3.amazonaws.com/arc-wordpress-client-uploads/infobae-wp/wp-content/uploads/2019/08/29205100/meme-gato-006.jpg'
+    },
+    {
+        id : 3,
+        title: 'Sabroso',
+        image: 'https://cdn.pocket-lint.com/r/s/1201x/assets/images/140427-apps-news-the-best-stupidest-and-most-famous-internet-memes-around-image1-lm1toysfy4.jpg'
+    },
+    {
+        id : 4,
+        title: 'Cat Crying',
+        image: 'https://pbs.twimg.com/media/ERAXSUqWAAA8ioK.jpg'
+    }
+]
+
+export default memes;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,33 +1,35 @@
 import { useState } from 'react';
 import meme from '../assets/meme-template.jpg'
-
+import memes from '../assets/memes.js';
 const Main = () => {
 
     
     const [topText, setTopText] = useState();
     const [botText, setBotText] = useState();
-
-
-
+    const [image,setImage] = useState(memes[Math.floor(Math.random() * memes.length)]);
+    const handlerSetImage = (e) =>{
+        e.preventDefault();
+       setImage(memes[Math.floor(Math.random() * memes.length)]);
+    }
     return (
         <main className="main">
             <form>
                 <div className="inputs--container">
                     <div className="left--input">
-                        <input type={'text'} required value={topText} onInput={e => setTopText(e.target.value.toUpperCase())} placeholder="Top Text"/>
+                        <input type={'text'}  value={topText} onChange={e => setTopText(e.target.value)} placeholder="Top Text"/>
                     </div>
                     <div className="right--input">
-                        <input type={'text'} required value={botText}  onInput={e => setBotText(e.target.value)} placeholder="Bottom Text" />
+                        <input type={'text'}  value={botText}  onChange={e => setBotText(e.target.value)} placeholder="Bottom Text" />
                     </div>
                 </div>
                 <div className="submit--container">
-                <input   className="submit"  type={'submit'} value="Get A new Image 🖼️"/>
+                <input onClick={e => handlerSetImage(e)}  className="submit"  type={'submit'} value="Get A new Image 🖼️"/>
                 </div>
             </form>
 
             <div className="meme--container">
                 <span className='top--text'>{topText}</span>    
-                <img src={meme} alt="MEME"/>
+                <img src={image.image} alt="MEME"/>
                 <span className='bottom--text'>{botText}</span>
             </div>
         </main>
